daf58601bebaac3be5248d0bd5794b99cf7125b7
Alison Feldish <alison.feldish@gmail.com>
2022-12-01 14:47:18 -0500

    styling

--- src/Coin.js
+++ src/Coin.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./Coin.css";
 
 class Coin extends Component {
     render() {
         return (
             <div className="coin">
-                <img src={this.props.data.imgSrc} alt={this.props.data.side} />;
+                <img src={this.props.data.imgSrc} alt={this.props.data.side} />
             </div>
         )
     };
